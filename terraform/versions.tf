070c081369db99e1c6a766ebddae4720c5788f58
Christian Kaatz <christian.kaatz@digitalservice.bund.de>
2023-06-30 15:19:27 +0200

    Update terraform provider versions

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     hcloud = {
       source = "hetznercloud/hcloud"
-      version = "~> 1.38"
+      version = "1.41"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 4.0"
+      version = "4.9.0"
     }
   }
   required_version = ">= 1.3.9"
